8b03abf555c62782ca258cbe6a010f92629c0cd5
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-05 13:52:15 +0000

    Add Nix-driven CI

---
+++ pkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/Holo-Host/holoportos/archive/8a415d4645fb764cbde53b47b8de87e2d01bc426.tar.gz";
+  sha256 = "16319sgdhv7z6j0qpc1qjkx4wxkhrz9lgqcx1my72l5az38am6cv";
+})
