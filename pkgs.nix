48b8546364189af208e6e89069b4acad0f157dbe
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-19 12:56:45 +0000

    Bump pkgs to a04ff82e194657ed6f02b187cae01347455d770c

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holoportos/archive/8a415d4645fb764cbde53b47b8de87e2d01bc426.tar.gz";
-  sha256 = "16319sgdhv7z6j0qpc1qjkx4wxkhrz9lgqcx1my72l5az38am6cv";
+  url = "https://github.com/Holo-Host/holoportos/archive/a04ff82e194657ed6f02b187cae01347455d770c.tar.gz";
+  sha256 = "1lmc1fr07fj2kqvbslj3wp75qd5fnn829l28z7y60zc70yax0sf0";
 })
